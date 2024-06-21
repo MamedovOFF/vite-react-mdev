c9d7e5696c26be6497ae75c4cb245d144d5568d8
M_dev <mamedov_dev@mail.ru>
2024-06-21 11:32:14 +0300

    Set Eslint rules

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Installation
+ make sure you have degit [degit](https://github.com/Rich-Harris/degit)
+```js
+degit git@github.com:MamedovOFF/repo
+```
+
 # React + TypeScript + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from 'react'
+
 function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  const [themeSwitch, setThemeSwitch] = useState(
+    window.matchMedia('(prefers-color-scheme: dark)').matches,
+  )
+
+  useEffect(() => {
+    if (themeSwitch) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [themeSwitch])
+
+  return (
+    <div className="h-full content-center bg-slate-100">
+      <div className="container m-auto">
+        <div className="transition text-center bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+          <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg" />
+          <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">
+            This is template for Vite+React project
+          </h3>
+          <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+            Basic settings (Eslint, Prettier, Tailwind, React Router Dom)
+          </p>
+          <button
+            className=" transition mt-2 p-2 rounded-md  dark:bg-white dark:text-black bg-slate-800 text-white"
+            type="button"
+            onClick={() => setThemeSwitch(!themeSwitch)}
+          >
+            Switch
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
